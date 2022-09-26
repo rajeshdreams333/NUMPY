f3f6c46477daac7f9bf9b65d21b5ebaecc215949
Rajeshdreams333 <rajeshdreams333@gmail.com>
2022-09-26 20:18:43 +0530

    Add all Dimensionals

--- dimensional.py
+++ dimensional.py
@@ -1,4 +1,21 @@
 # 0-D
 import numpy as np
 arr=np.array(33)
-print(arr)
+# print(arr)
+
+# 1-D
+import numpy as np
+arr1=np.array([8,0,9,6,1,9,4,1,2,3])
+# print(arr1)
+
+# 2-D
+arr2=np.array([[9,8,8],[5,9,5],[2,3,4]])
+# print(arr2)
+
+arr3=np.array([[[1,2,3],[4,5,6]],[[1,2,3],[0,0,0]]])
+# print(arr3)
+
+print(arr.ndim)
+print(arr1.ndim)
+print(arr2.ndim)
+print(arr3.ndim)

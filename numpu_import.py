171022b24ab086c5824603b27e0d6416d753834b
RAJESH <60741789+rajeshdreams333@users.noreply.github.com>
2022-09-27 13:00:19 +0530

    Add files via upload
    this is 1st rajesh branch

---
+++ numpu_import.py
@@ -0,0 +1,13 @@
+# import numpy
+# arr=numpy.array([1,2,3,4,5])
+# print(arr)
+
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(arr)
+print(np.__version__)
+print(type(arr))
+
+# use tuple to create numpy
+r=np.array((1,2,5,3,10))
+print(r)
